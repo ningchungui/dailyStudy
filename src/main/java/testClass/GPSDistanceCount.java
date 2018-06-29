9be7c5fd03e230f39d4e3a1440a4a6af32e7cd17
ningchungui <1248884673@qq.com>
2018-06-29 16:20:41 +0800

    Initial commit

---
+++ src/main/java/testClass/GPSDistanceCount.java
@@ -0,0 +1,30 @@
+package testClass;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Create by NING on 2018/6/29.<br>
+ */
+public class GPSDistanceCount {
+    public static void main(String[] args) throws Exception {
+
+        FileReader fileReader = new FileReader("D:\\2610579用户行车记录.csv");
+        BufferedReader reader = new BufferedReader(fileReader);
+
+        for(;;){
+            String line = reader.readLine();
+            if (line == null) break;
+            String[] split = line.split(",", 4);
+            String gps = StringUtils.strip(split[4], "\"");
+
+            System.out.println("字段1-->"+split[0] +"   字段2-->"+split[1]+"   字段3-->"+split[2]+"   字段4-->"+split[3]);
+        }
+
+
+
+    }
+
+}
---
+++ src/main/java/util/GPS.java
@@ -0,0 +1,26 @@
+package util;
+
+/**
+ * Create by NING on 2018/6/29.<br>
+ */
+public class GPS {
+    private static double EARTH_RADIUS = 6378.137;//地球半径
+    private static double rad(double d){
+        return d*Math.PI/180;
+    }
+
+    public static double GetDistance(double lat1, double lng1, double lat2, double lng2){
+        double a = rad(lat1) - rad(lat2);
+        double b = rad(lng1) - rad(lng2);
+
+        double s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a/2),2) +
+                Math.cos(rad(lat1))*Math.cos(rad(lat2))*Math.pow(Math.sin(b/2),2)));
+        s = s * EARTH_RADIUS;
+        s = Math.round(s * 10000) / 10000;
+        return s;
+
+    }
+
+
+
+}
